3166372d326a0db74211a9664956f37d58c751fe
TERRAS Juba <ij_terras@esi.dz>
2022-03-25 18:34:41 +0100

    Rectification pool bdd

--- config/bd.js
+++ config/bd.js
@@ -1,20 +1,20 @@
 const Pool = require('pg').Pool
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'utilisateur_autotek@serveurautotekbdd',
   host: 'serveurautotekbdd.postgres.database.azure.com',
   database: 'autotek',
   password: 'Autotek2022*',
   port: 5432,
-})
+})*/
 
-/*const pool = new Pool({
+const pool = new Pool({
   user: 'postgres_admin@serveurautotekbdd',
   host: 'serveurautotekbdd.postgres.database.azure.com',
   database: 'autotek',
   password: 'Autotek2022*',
   port: 5432,
-})*/
+})
 
 /*const pool = new Pool({
   user: 'postgres',
