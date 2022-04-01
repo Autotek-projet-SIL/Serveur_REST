28a139d8d1b111fb729bd3319548f3e38f7b8b7c
Khaoula Mehar <ik_mehar@esi.dz>
2022-04-01 18:15:15 +0200

    version finale des tests

--- spec/TestGestionComptes.spec.js
+++ spec/TestGestionComptes.spec.js
@@ -44,7 +44,7 @@ describe("Tester le service GestionComptes", () => {
             "id": "idajoute",
             "nom": "Mechouar",
             "prenom": "MANEL",
-            "email": "ik_mehar@esi.dz",
+            "email": "im_mehouar@esi.dz",
             "mot_de_passe": "kjl28vcn",
             "numero_telephone": "0645321321",
             "est_root": false,
@@ -61,12 +61,12 @@ describe("Tester le service GestionComptes", () => {
         
             
          await   axios
-                .get(url + 'authentification_web/atc_connexion/ik_mehar@esi.dz')
+                .get(url + 'authentification_web/atc_connexion/im_mehouar@esi.dz')
                 .then(res1 => {
 
                 
                 
-                        expect(res1.data[0].email).toEqual("ik_mehar@esi.dz")
+                        expect(res1.data[0].email).toEqual("im_mehouar@esi.dz")
                         expect(res1.data[0].id_atc).toEqual("idajoute")
                    
                
@@ -77,9 +77,9 @@ describe("Tester le service GestionComptes", () => {
         let data = {
             "token": "avPraesPu0hkkvlsRaHhcGx3VSph2",
             "id": "idajoute",
-            "nom": "Mehar",
-            "prenom": "Khaoula",
-            "email": "ik_mehar@esi.dz",
+            "nom": "Brahimi",
+            "prenom": "Amina",
+            "email": "brahimi@gmail.com",
             "mot_de_passe": "kjl28vcn",
             "numero_telephone": "0645321321",
             "photo_am": "ppppp"              
@@ -95,12 +95,12 @@ describe("Tester le service GestionComptes", () => {
         
             
          await   axios
-                .get(url + 'authentification_mobile/am_connexion/ik_mehar@esi.dz')
+                .get(url + 'authentification_mobile/am_connexion/brahimi@gmail.com')
                 .then(res1 => {
 
                 
                 
-                        expect(res1.data[0].email).toEqual("ik_mehar@esi.dz")
+                        expect(res1.data[0].email).toEqual("brahimi@gmail.com")
                         expect(res1.data[0].id_am).toEqual("idajoute")
                    
                
@@ -140,7 +140,7 @@ describe("Tester le service GestionComptes", () => {
            
                
             await   axios
-                   .get(url + 'authentification_web/atc_connexion/ik_mehar@esi.dz')
+                   .get(url + 'authentification_web/atc_connexion/im_mehouar@esi.dz')
                    .then(res => {
    
                    
@@ -162,7 +162,49 @@ describe("Tester le service GestionComptes", () => {
            
                
             await   axios
-                   .get(url + 'authentification_mobile/am_connexion/ik_mehar@esi.dz')
+                   .get(url + 'authentification_mobile/am_connexion/brahimi@gmail.com')
+                   .then(res => {
+   
+                   
+                   
+                       expect(res.data[0]).not.toBeDefined();
+                      
+                  
+           });
+       });
+
+
+       it("Tester la suppression d'un locataire", async() => {
+        let data = {
+            "token": "avPraesPu0hkkvlsRaHhcGx3VSph2",
+            "id": "idajoute",
+            "nom": "Madani",
+            "prenom": "Nassim",
+            "email": "madaninassim@gmail.com",
+            "mot_de_passe": "kjl28vcn",
+            "numero_telephone": "0645321321",
+            "photo_identite_recto": "nassim_photo",
+            "photo_identite_verso": "nassim_photo",
+            "photo_selfie": "nassim_photo",
+            "statut_compte": "false",
+            "statut": "en attente",
+            "date_inscription": "2022-03-30"
+        }
+     await   axios.post(url + 'authentification_mobile/locataire_inscription/', data
+        )
+            .then(res => {
+                expect(res.status).toEqual(200)
+            })
+    
+        await    axios.delete(url + 'gestioncomptes/supprimer_locataire/idajoute'
+        )
+            .then(res => {
+                expect(res.status).toEqual(200)
+        });
+           
+               
+            await   axios
+                   .get(url + 'authentification_mobile/locataire_connexion/madaninassim@gmail.com')
                    .then(res => {
    
                    
