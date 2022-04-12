46b93a1b8cdb94c47ab27157203862d27489e91d
TERRAS Juba <ij_terras@esi.dz>
2022-04-12 20:55:56 +0100

    Ajustement de la fonctionalitée d'envois de notification

--- config/firebase.js
+++ config/firebase.js
@@ -3,34 +3,14 @@ const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
 let admin = require("firebase-admin");
 let serviceAccount = require("./autotek-8c725-firebase-adminsdk-7tu4s-24ed0288bc.json");
-let FCM = require("fcm-node");
-
-// Adresse du serveur de cloud messaging
-let serverKey =
-  "AAAATUL1ohc:APA91bGtBYeGQzb7lXlGOfyXwv3YIOwhGNnQ88aGL3HeBexcHnZ9XJHUuwWKUFAyS7IZn4vTwbWeIGxZfH29Ta69zoesutvyHJVXUrzBKNrj3B5seMaWmOpaT0DvnOHkbJoQ9GA7ZtDd";
-let fcm = new FCM(serverKey);
-
+const log = require("../config/Logger");
 // Initisaliser l'admin SDK de FireBase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-// Informations de la configuration de l'application firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCPt_6W95_a63qCoapur-C9mzz9uJGG1uY",
-  authDomain: "autotek-8c725.firebaseapp.com",
-  projectId: "autotek-8c725",
-  storageBucket: "autotek-8c725.appspot.com",
-  messagingSenderId: "331835875863",
-  appId: "1:331835875863:web:196d016fc1488af40c7252",
-  measurementId: "G-67MEGP9H81",
-};
-
 // Initialiser Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const db = admin.firestore();
-
+const messaging = admin.messaging();
 // Fonction de verification des tokens de Firebase
 const verifyToken = async (request) => {
   if (process.env.NODE_ENV === "production") {
@@ -56,6 +36,8 @@ const verifyToken = async (request) => {
           }
         })
         .catch((error) => {
+          log.loggerConsole.error(error);
+          log.loggerFile.error(error);
           reject(new Error("Requete refusée"));
         });
     });
@@ -66,23 +48,46 @@ const verifyToken = async (request) => {
 const sendNotification = async (title, body, request, response) => {
   const user = await db.collection("DeviceToken").doc(request.body.id).get();
   if (user.exists) {
-    const registrationToken = user.data()["device_token"];
-    console.log(registrationToken);
-    await admin
+    let registrationToken = await user.data()["device_token"];
+    registrationToken=registrationToken.replace(/\s/g, '')
+    /*admin
       .messaging()
-      .sendMulticast({
-        tokens: [registrationToken],
-        notification: {
-          title: "Weather Warning!",
-          body: "A new weather warning has been issued for your location.",
-          imageUrl: "https://my-cdn.com/extreme-weather.png",
+      .send({
+        token: registrationToken,
+        data: {
+          hello: "world",
         },
+        // Set Android priority to "high"
+        android: {
+          priority: "high",
+        },
+        // Add APNS (Apple) config
       })
       .then((response) => {
         console.log("Successfully sent message:", response);
       })
       .catch((error) => {
         console.log("Error sending message:", error);
+      });*/
+      
+      var payload = {
+      notification: {
+        title: title,
+        body: body,
+      },
+    };
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24,
+    };
+    await messaging
+      .sendToDevice(registrationToken, payload, options)
+      .then(function (response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function (error) {
+        log.loggerConsole.error(error);
+        log.loggerFile.error(error);
       });
   }
 };
