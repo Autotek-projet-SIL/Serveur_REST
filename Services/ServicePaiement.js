f8f6c0af442e84b5a3f8ac42be4849340e0ca6f1
TERRAS Juba <ij_terras@esi.dz>
2022-05-26 00:35:32 +0100

    Integration du service de paiement stripe

--- Services/ServicePaiement.js
+++ Services/ServicePaiement.js
@@ -1,8 +1,10 @@
 const log = require("../config/Logger");
 let { PythonShell } = require("python-shell");
 const modelPayer = require("../Models/ModelPayer");
+const modelLocataire = require("../Models/ModelLocataire");
+const { Convert } = require("easy-currencies");
 const stripe = require("stripe")(
-  "sk_test_51KtJjXD9GU8MlLbuWhkRfcxgL14ibLuHaGIpsv668b4l5lfsclV1FZiyOctezrUjYOJJWrHvMJbu7M8j8CgroAK000Kv10JXg0"
+  "sk_test_51L3RzUAMbIvDVipZyYnCwUWg5M2yHCqzfOBzURCHhVkBjg5RdiVksQXQpyd35Nw0S6snKNo605yIyhwYSFUOWnH000utsLGxOa"
 );
 
 // Fonction de verification de validite du paiement
@@ -30,17 +32,56 @@ const VerifierPaiement = async (request, response) => {
       }
     });
   } else if (type_paiement === "stripe") {
-    const paymentIntent = await stripe.paymentIntents.retrieve(id_transaction);
-    if (
-      paymentIntent.status === "succeeded" &&
-      paymentIntent.amount == montant
-    ) {
-      response.sendStatus(200);
+    let customerId = null;
+    let customer = null;
+    let balance = null;
+    let amount = await Convert(request.body.montant).from("DZD").to("EUR");
+    amount = parseInt(amount) * 100;
+    let email = request.body.email;
+    let data = await modelLocataire.getLocaireStripeIdByEmail(email);
+    if (data["stripe_id"] == null) {
+      customer = await stripe.customers.create({
+        email: email,
+        balance: 10000,
+      });
+      await modelLocataire.updateLocataireStripeId(email, customer.id);
+      customerId = customer.id;
     } else {
-      response.sendStatus(406);
+      customerId = data["stripe_id"];
+      customer = await stripe.customers.retrieve(customerId);
+      balance = customer.balance;
     }
-  } else {
-    response.sendStatus(406);
+    try {
+      const card_Token = await stripe.tokens.create({
+        card: {
+          name: request.body.name,
+          number: request.body.numero_card,
+          exp_month: request.body.exp_month,
+          exp_year: request.body.exp_year,
+          cvc: request.body.cvc,
+        },
+      });
+      const card = await stripe.customers.createSource(customerId, {
+        source: `${card_Token.id}`,
+      });
+      if (amount > balance) {
+        response.sendStatus(406);
+      } else {
+        await stripe.customers.createBalanceTransaction(customerId, {
+          amount: -amount,
+          currency: "eur",
+        });
+        await stripe.customers.createBalanceTransaction("cus_Lky8p8VrgnpaVR", {
+          amount: amount,
+          currency: "eur",
+        });
+        await modelPayer.addPaiement(request, response);
+      }
+    } catch (error) {
+      log.loggerConsole.error(error);
+      log.loggerFile.error(error);
+      response.sendStatus(500);
+    }
   }
 };
 
