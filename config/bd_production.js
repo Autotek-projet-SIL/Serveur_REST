91077efe2d1a97d337494282328709b6e9766cf9
TERRAS Juba <ij_terras@esi.dz>
2022-06-26 10:55:38 +0100

    Test variables d'environnement heroku

--- config/bd_production.js
+++ config/bd_production.js
@@ -1,6 +1,5 @@
 // DataBase configuration when the environnement mode is production ( cloud database )
 const Pool = require("pg").Pool;
-
 // Open a connection with the database en production mode
 const poolProduction = new Pool({
   connectionString: process.env.DATABASE_URL,
