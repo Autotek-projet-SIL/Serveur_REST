083172c5294d79fb868a9ca00faa68b231e48fda
TERRAS Juba <ij_terras@esi.dz>
2022-06-26 10:47:51 +0100

    Test variables d'environnement heroku

--- config/bd_test_integration.js
+++ config/bd_test_integration.js
@@ -3,12 +3,10 @@ const Pool = require("pg").Pool;
 
 // Open a connection with the database en local mode
 const poolTestIntegration = new Pool({
-  user: process.env.db_user,
-  host: process.env.db_host,
-  database: process.env.db_name,
-  password: process.env.db_password,
-  port: process.env.db_port,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 // export the connection method
 module.exports = poolTestIntegration;
