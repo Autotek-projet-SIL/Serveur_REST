0324d65409798d7604475e8497f4fa1086613e56
TERRAS Juba <ij_terras@esi.dz>
2022-06-26 10:48:42 +0100

    Test variables d'environnement heroku

--- config/bd_production.js
+++ config/bd_production.js
@@ -2,12 +2,11 @@
 const Pool = require("pg").Pool;
 
 // Open a connection with the database en production mode
-const poolProduction = new Pool({
-  user: process.env.db_user,
-  host: process.env.db_host,
-  database: process.env.db_name,
-  password: process.env.db_password,
-  port: process.env.db_port,
+const poolTestIntegration = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // export the connection method
